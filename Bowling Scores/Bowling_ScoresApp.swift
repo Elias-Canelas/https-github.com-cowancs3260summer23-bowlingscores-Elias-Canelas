a056af496b81618157aa285e25fa2705b64a0745
Elias-Canelas <eliasrojas3535@gmail.com>
2023-06-03 16:20:24 -0600

    Initial Commit

---
+++ Bowling Scores/Bowling_ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bowling_ScoresApp.swift
+//  Bowling Scores
+//
+//  Created by Elias on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Bowling_ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
